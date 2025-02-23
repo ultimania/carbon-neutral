a158f0ab3e3ff154bb19a94d78457bc8d35c4f37
ultimania <fukaya-takumi@sysj.co.jp>
2025-02-23 14:50:43 +0900

    fix: require login

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,36 @@
 "use client";
 
-import Image from "next/image";
+import Login from "@/components/Login";
+import { Button } from "@/components/ui/Button";
+import Header from "@/components/ui/Header";
+import Sidebar from "@/components/ui/Sidebar";
+import { HelpCircleIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
-import Login from "../components/Login";
-import Logout from "../components/Logout";
 
-export default function Home() {
+export default function Home({ children }: { children: React.ReactNode }) {
   const { data: session } = useSession();
-
+  if (!session) {
+    return <Login />;
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {!session ? <Login /> : <Logout />}
-      </main>
+    <div className="flex h-screen bg-gray-100 text-primary-text">
+      {/* Sidebar */}
+      <Sidebar />
+
+      <div className="w-screen">
+        <Header />
+        <main className="flex flex-col p-4 justify-center">
+          {children}
+        </main>
+      </div>
+
+      {/* Help button */}
+      <div className="fixed bottom-4 right-4">
+        <Button className="bg-teal-500 hover:bg-teal-600 text-white">
+          <HelpCircleIcon className="mr-2" size={18} />
+          Need help?
+        </Button>
+      </div>
     </div>
   );
 }
